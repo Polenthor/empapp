056a72faded8e1240ef7ad2f2b66eb5795346b01
Polenthor <shiduaboobacker@gmail.com>
2026-04-29 20:06:33 +0400

    ass

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: API,
-  withCredentials: true // optional (only if using cookies)
+  baseURL: API
 });
 
 export default api;
